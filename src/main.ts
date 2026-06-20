f4e514c004d0ac826a65035fb99d340f704f7011
Дмитрий Крюков <demob@MacBook-Pro-Dmitrij.local>
2026-06-20 17:32:36 +0400

    =Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+alert('Hello, world!');
+//import Phaser from 'phaser';
+//import './styles/global.css';
+//import { GameConfig } from './GameConfig';
+
+//const game = new Phaser.Game(GameConfig);
+
+//(window as unknown as { __armoryGame?: Phaser.Game }).__armoryGame = game;
